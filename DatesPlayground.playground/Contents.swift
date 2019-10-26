d38bf3d3530b5ecd9cf3cbc9cf62bd9fc4a1a150
Russell Mirabelli <russell@mirabellifamily.org>
2019-10-26 13:53:29 -0500

    Initial commit

---
+++ DatesPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// Start with a date, like so:
+
+let date = Date()
+
+// Create a date using today's date and a DateComponents structure
+
+// Compare the two dates to see if they occur in the same Calendar day
+
+// Use a date formatter
+
+let formatter = DateFormatter()
+
+// use the formatter to print the day in a friendly format.
+// hint, use NSDateFormatter.com to get an appropriate printing format string.
---
+++ README.md
@@ -0,0 +1,6 @@
+# uofdmobile_homework5
+Homework 5 template for UofD mobile apps class
+
+This should be a very simple assignment. Its purpose is to work through the process for manipulating dates in Swift, so you have a good understanding when working on Project 2.
+
+Follow the instructions in the playground.
