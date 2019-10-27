1c29daab61482f375a36f3372f4c33bfc3c79a1f
Zainab <zalibhai2121@gmail.com>
2019-10-27 13:46:29 -0500

    finished

--- DatesPlayground.playground/Contents.swift
+++ DatesPlayground.playground/Contents.swift
@@ -3,7 +3,7 @@ import Foundation
 // Start with a date, like so:
 
 let date = Date()
-
+let nextweek = date.addingTimeInterval(7*24*60*60)
 // Create a date using today's date and a DateComponents structure
 
 // Compare the two dates to see if they occur in the same Calendar day
@@ -11,6 +11,14 @@ let date = Date()
 // Use a date formatter
 
 let formatter = DateFormatter()
+formatter.dateFormat = "EEEE, MMM, d, yyyy"
+print(formatter.string(from: date))
+
+if Calendar.current.isDate(date, inSameDayAs: nextweek){
+    print("yes")
+} else {
+    print("no")
+}
 
 // use the formatter to print the day in a friendly format.
 // hint, use NSDateFormatter.com to get an appropriate printing format string.
